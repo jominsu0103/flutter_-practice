cb2987d5f945dc1c3210138957ded3fe1dcedc6c
usnimoj <jominsu0103@naver.com>
2024-04-07 01:05:33 +0900

    flutter theme

---
+++ lib/style/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final customTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+    textTheme: const TextTheme(
+        bodyLarge: TextStyle(fontWeight: FontWeight.normal, fontSize: 30)),
+    useMaterial3: false);
